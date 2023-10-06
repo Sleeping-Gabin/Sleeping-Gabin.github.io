cb567be4f9186dd5aa97ca9ed6bca8d1b1066178
gabin <bini020408@naver.com>
2023-10-06 21:18:22 +0900

    Update masthead.js

--- js/masthead.js
+++ js/masthead.js
@@ -1,4 +1,22 @@
 $(function () {
+    changeMastheadDisplay();
+
+    $("#nav-button").click(function() {
+        $("#nav").slideToggle();
+
+        if ($("#nav-button").text() == "arrow_drop_up") {
+            $("#nav-button").text("arrow_drop_down");
+        }
+        else {
+            $("#nav-button").text("arrow_drop_up");
+        }
+    });
+
+    $(window).resize(changeMastheadDisplay);
+
+});
+
+var changeMastheadDisplay = function() {
     var path = window.location.pathname;
     var width = window.innerWidth;
 
@@ -12,30 +30,4 @@ $(function () {
         $("#nav").show();
         $("#nav-button").hide();
     }
-
-    $("#nav-button").click(function() {
-        $("#nav").slideToggle();
-
-        if ($("#nav-button").text() == "arrow_drop_up") {
-            $("#nav-button").text("arrow_drop_down");
-        }
-        else {
-            $("#nav-button").text("arrow_drop_up");
-        }
-    });
-
-    $(window).resize(function() {
-        if (path != "/" && width <= 640) {
-            $(".site-avatar").hide();
-            $("#nav").hide();
-            $("#nav-button").show();
-        }
-        else {
-            $(".site-avatar").show();
-            $("#nav").show();
-            $("#nav-button").hide();
-        }
-    });
-
-});
-
+}
